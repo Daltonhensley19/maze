122137845f6ebc06cbca48a0c499c45088f43933
Daltonhensley19 <daltonhensley2000@gmail.com>
2025-05-02 18:16:16 -0400

    Move `MazeError` into its own source file

---
+++ src/error.rs
@@ -0,0 +1,12 @@
+use std::path::PathBuf;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MazeError {
+    #[error("The project name `{0}` already exists in the current directory")]
+    ProjectExists(PathBuf),
+
+    #[error("The CMakeLists.txt file does not exist in the project directory")]
+    CMakeFileNotFound,
+}
